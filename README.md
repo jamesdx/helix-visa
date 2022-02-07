e9fbffd8f846ada69548bbd222a127baf8dc75fa
allenthinking@outlook.com <Win2020@>
2022-02-07 21:16:22 +0800

    HV-SB-001: 如何构建一个 SpringBoot Maven 基础项目

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # helix-visa
+## helix-visa-spring-single
+如何使用 Spring Boot
---
+++ helix-visa-springboot-single/src/main/java/com/helix/visa/springboot/HelixVisaSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.helix.visa.springboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelixVisaSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelixVisaSpringBootApplication.class);
+    }
+}
